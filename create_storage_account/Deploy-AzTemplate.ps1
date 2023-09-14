dbd65be239c4e11782d2d5c88df0219af46be6fd
Suchitha Malisetty <suchithamalisetty@gmail.com>
2023-09-14 18:16:22 +0530

    updated script with parameter file

--- create_storage_account/Deploy-AzTemplate.ps1
+++ create_storage_account/Deploy-AzTemplate.ps1
@@ -5,8 +5,8 @@ $resourceGroupName = "ResourceDeployment"
 # $storageAccountName = "suchistorageaccount"
 
 # Deploy the Azure Storage Account using ARM template and parameters
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile create_storage_account/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile create_storage_account/azuredeploy.json -TemplateParameterFile create_storage_account/azuredeploy.parameters.json
 
 #print after deployment
-# $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
-# Write-Host "Storage Account Name: $($storageAccount.StorageAccountName)"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+Write-Host "Storage Account Name: $($storageAccount.StorageAccountName)"
