e35fc0d4410c411ad03c7b0bf1d931413e074ed9
Suchitha Malisetty <suchithamalisetty@gmail.com>
2023-09-14 11:27:00 +0530

    updated scripts and templates

--- create_storage_account/Deploy-AzTemplate.ps1
+++ create_storage_account/Deploy-AzTemplate.ps1
@@ -2,27 +2,11 @@ Connect-AzAccount
 
 # Define variables
 $resourceGroupName = "ResourceDeployment"
-$storageAccountName = "azureStorageAccount"
-$location = "Central India" 
-$storageAccountSku = "Standard_LRS" 
+$storageAccountName = "suchistorageaccount"
 
 # Deploy the Azure Storage Account using ARM template and parameters
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
-    -TemplateFile .\azuredeploy.json `
-    -TemplateParameterFile .\azuredeploy.parameters.json `
-    -storageAccountName $storageAccountName `
-    -location $location `
-    -storageAccountSku $storageAccountSku
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "C:\Users\smalisetty\OneDrive - Microsoft\Suchi\ARM\ARMPipeline\create_storage_account\azuredeploy.json"
 
-# Check deployment status
-$deployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name (Get-Date -UFormat "%Y%m%d%H%M%S")
-
-if ($deployment.ProvisioningState -eq "Succeeded") {
-    Write-Host "Azure Storage Account deployment succeeded."
-    $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
-    Write-Host "Storage Account Name: $($storageAccount.StorageAccountName)"
-    Write-Host "Storage Account Key1: $($storageAccount.Keys[0].Value)"
-} else {
-    Write-Host "Azure Storage Account deployment failed."
-    Write-Host "Details: $($deployment.StatusMessage)"
-}
+#print after deployment
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+Write-Host "Storage Account Name: $($storageAccount.StorageAccountName)"
